8ce374b75545b1e894638dd4c15a476a175c500d
Esenou <superesenou.kg@gmail.com>
2018-11-15 12:17:32 +0600

    Second commit

---
+++ app/src/main/java/app/superesenou/ru/myapplicationtwitter/MainActivity.kt
@@ -0,0 +1,60 @@
+package app.superesenou.ru.myapplicationtwitter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.widget.Toast
+import com.twitter.sdk.android.core.TwitterAuthToken
+import com.twitter.sdk.android.core.TwitterCore
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Twitter.initialize(this);
+        setContentView(R.layout.activity_main)
+
+        login_button.callback = object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>?) {
+                val session = TwitterCore.getInstance().sessionManager.activeSession
+                val authToken = session.authToken
+                val token = authToken.token
+                val secret = authToken.secret
+                login(session)
+            }
+            override fun failure(exception: TwitterException?) {
+
+                Toast.makeText(this@MainActivity,"Authentication Failed!",Toast.LENGTH_LONG).show()
+
+            }
+
+
+        }
+    }
+
+    fun login(session:TwitterSession){
+        var username:String=session.userName
+        var id_user: Long? =session.userId
+        val intent=Intent(this,HomeActivity::class.java)
+        val bundle:Bundle
+        intent.putExtra("key",id_user)
+        intent.putExtra("username",username)
+
+        startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result to the login button.
+        login_button.onActivityResult(requestCode, resultCode, data)
+    }
+}
